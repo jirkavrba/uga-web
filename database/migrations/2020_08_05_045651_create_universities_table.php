72b2ad327d850b001d5a36d4b9f71e7fb84d2592
Jiří Vrba <jiri@vrba.dev>
2020-08-05 07:01:50 +0200

    Basic university model

--- database/migrations/2020_08_05_045651_create_universities_table.php
+++ database/migrations/2020_08_05_045651_create_universities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUniversitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('universities', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->boolean("is_admin")->default(false);
-            $table->integer("university_id");
-            $table->rememberToken();
+            $table->string("name")->unique();
+            $table->string("color");
+            $table->string("logo_url");
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('universities');
     }
 }
